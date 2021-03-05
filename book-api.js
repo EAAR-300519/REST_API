10e726036bf91e70c1b122c40c41e247608967e3
EAAR-300519 <77472299+EAAR-300519@users.noreply.github.com>
2021-03-04 21:17:42 -0600

    Agregamos cambios

--- book-api.js
+++ book-api.js
@@ -15,13 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post('/book', (req, res) => {
-    const book = req.body;
-
-    // Output the book to the console for debugging
-    console.log(book);
-    books.push(book);
-
-    res.send('Book is added to the database');
+    // We will be coding here
 });
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
